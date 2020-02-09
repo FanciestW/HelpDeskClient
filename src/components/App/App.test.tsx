dd839034b35e5c50751bf1db32dccc1562bdf535
William Lin <wlin26@yahoo.com>
2020-02-09 15:17:39 -0500

    Updated test to pass

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/dashboard/i);
-  expect(linkElement).toBeInTheDocument();
+  const { getByLabelText } = render(<App />);
+  const menuButton = getByLabelText('menu');
+  expect(menuButton).toBeInTheDocument();
 });
