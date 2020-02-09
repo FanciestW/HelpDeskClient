4f68bf19da25a78ee5df660e534c7b1238275795
William Lin <wlin26@yahoo.com>
2020-02-09 13:47:48 -0500

    Added navbar for dashboard

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Dashboard from '../Dashboard/Dashboard';
+import Navbar from '../Navbar/Navbar';
 
 const App = () => {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <Dashboard></Dashboard>
     </div>
   );
