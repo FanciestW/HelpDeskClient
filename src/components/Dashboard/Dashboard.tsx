3933559bb0717671919ad09300d791df9eb8ff44
William Lin <wlin26@yahoo.com>
2020-02-09 19:44:38 -0500

    Adding paper cards for dashboard

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,8 +1,37 @@
 import React from 'react';
+import { Grid, Paper } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    height: 280,
+    width: 240,
+  },
+}));
 
 const Dashboard = () => {
+  const classes = useStyles();
   return (
-    <h1>Dashboard</h1>
+    <div className={classes.root}>
+      <Grid container className={classes.root} spacing={2}>
+        <Grid item xs={12}>
+          <Grid container justify="center" spacing={1}>
+            <Grid key={0} item>
+              <Paper className={classes.paper} />
+            </Grid>
+            <Grid key={1} item>
+              <Paper className={classes.paper} />
+            </Grid>
+            <Grid key={2} item>
+              <Paper className={classes.paper} />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    </div>
   );
 };
 
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppBar, Toolbar, IconButton, Typography, Button } from '@material-ui/core';
+import { AppBar, Toolbar, IconButton, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { Menu as MenuIcon } from '@material-ui/icons';
 
