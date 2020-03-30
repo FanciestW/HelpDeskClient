426af51bbafa03d101597d9e7efd43922bbb440a
William Lin <wlin26@yahoo.com>
2020-03-30 15:52:49 -0400

    Fixed notfound to support dark theme and cleaned up styling

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Typography } from '@material-ui/core';
 
 import travolta from '../../assets/confused_travolta.gif';
 
 export default function NotFound() {
   return (
-    <div style={{textAlign: 'center',}}>
-      <h1>404</h1>
-      <h2>Page Not Found</h2>
+    <div style={{textAlign: 'center', margin: '20px'}}>
+      <Typography variant='h3' component='h3' color='textPrimary'>404</Typography>
+      <Typography variant='h4' component='h4' color='textPrimary'>Page Not Found</Typography>
       <img style={{position: 'fixed', left: 'calc(50% - 238px)', bottom: '0px'}} src={travolta} alt="Sorry..." />
     </div>
   );
