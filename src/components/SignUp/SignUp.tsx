4cc2794ebedfa3e18089795f496a6beb8a450912
William Lin <wlin26@yahoo.com>
2020-04-11 21:50:12 -0400

    Added states for each field

--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -1,43 +1,157 @@
-import React from 'react';
-import { Button, Grid, Paper, TextField, Typography } from '@material-ui/core';
+import React, { useState } from 'react';
+import {
+  Button,
+  Grid,
+  TextField,
+  Typography,
+  FormLabel,
+  FormControl,
+  RadioGroup,
+  Radio,
+  Container,
+  CssBaseline,
+  FormControlLabel,
+  Link
+} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Avatar from '@material-ui/core/Avatar';
 
-const useStyles = makeStyles(() => ({
-  loginPaper: {
-    padding: '40px 20px',
-  }
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
 }));
 
 export default function SignUp() {
   const classes = useStyles();
+  const [isTechnician, setIsTechnician] = useState('no');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleIsTechnician = (event: any) => {
+    setIsTechnician(event.target.value);
+  };
+  
   return (
-    <Grid container
-      spacing={0}
-      direction='column'
-      alignItems='center'
-      justify='center'
-    >
-      <Grid item xs style={{ maxWidth: '100%' }}>
-        <Paper className={classes.loginPaper}>
-          <Grid container
-            spacing={2}
-            direction='column'
-            alignItems='center'
-            justify='center'
-            style={{ width: '50vw' }}
-          >
-            <Grid item xs>
-              <Typography variant='h3' component='h3' color='textPrimary'>Sign Up</Typography>
+    <Container component='main' maxWidth='xs'>
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component='h1' variant='h5'>
+          Sign up
+        </Typography>
+        <form className={classes.form} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField autoFocus
+                name='firstName'
+                variant='outlined'
+                required
+                fullWidth
+                id='firstName'
+                label='First Name'
+                autoComplete='family-name'
+                onChange={(event: any) => setFirstName(event.target.value)}
+              />
             </Grid>
-            <Grid item xs>
-              <TextField fullWidth label='First Name' />
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant='outlined'
+                name='lastName'
+                required
+                fullWidth
+                id='lastName'
+                label='Last Name'
+                autoComplete='given-name'
+                onChange={(event: any) => setLastName(event.target.value)}
+              />
             </Grid>
-            <Grid item xs>
-              <Button>Sign up</Button>
+            <Grid item xs={12}>
+              <TextField
+                variant='outlined'
+                required
+                fullWidth
+                id='email'
+                label='Email Address'
+                name='email'
+                autoComplete='email'
+                onChange={(event: any) => setEmail(event.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant='outlined'
+                fullWidth
+                name='phone'
+                label='Phone Number (Optional)'
+                type='tel'
+                id='phone'
+                autoComplete='tel'
+                onChange={(event: any) => setPhone(event.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant='outlined'
+                required
+                fullWidth
+                name='password'
+                label='Password'
+                type='password'
+                id='password'
+                autoComplete='current-password'
+                value={password}
+                onChange={(event: any) => setPassword(event.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FormControl component="fieldset">
+                <FormLabel component="legend">Are You a Technician</FormLabel>
+                <RadioGroup aria-label="isTechnician" name="isTechnician" value={isTechnician} onChange={handleIsTechnician}>
+                  <FormControlLabel value="yes" control={<Radio color='primary'/>} label="Yes" />
+                  <FormControlLabel value="no" control={<Radio color='primary'/>} label="No" />
+                </RadioGroup>
+              </FormControl>
             </Grid>
           </Grid>
-        </Paper>
-      </Grid>
-    </Grid>
+          <Button
+            type='submit'
+            fullWidth
+            variant='contained'
+            color='primary'
+            className={classes.submit}
+          >
+            Sign Up
+          </Button>
+          <Grid container justify='flex-end'>
+            <Grid item>
+              <Link href='/login' variant='body2'>
+                Already have an account? Log in
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
   );
 }
