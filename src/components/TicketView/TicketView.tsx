19f640f7b0473dea6a4571c460cc9110a76c25e7
William Lin <wlin26@yahoo.com>
2020-04-23 00:34:38 -0400

    Added MUIDataTable component to ticket view

--- src/components/TicketView/TicketView.tsx
+++ src/components/TicketView/TicketView.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useQuery, gql, ApolloError, ServerParseError } from '@apollo/client';
-import { changeAuthed } from '../../redux/actions/AuthedActions'
+import MUIDataTable from 'mui-datatables';
+import { changeAuthed } from '../../redux/actions/AuthedActions';
+import ITicket from '../../interfaces/Ticket';
 
 export default function TicketView() {
   const dispatch = useDispatch();
@@ -29,10 +31,27 @@ export default function TicketView() {
       }
     }
   `;
-  const [ticketsData, setTicketsData] = React.useState([]);
+  const [ticketsData, setTicketsData] = useState([['']]);
   useQuery(query, {
-    onCompleted: (data) => {
-      setTicketsData(data.getTickets);
+    onCompleted: (data: { getTickets: ITicket[]}) => {
+      const formattedData = data?.getTickets?.map((tix: ITicket): string[] => {
+        let ticketDueDate = 'No Date';
+        try {
+          ticketDueDate = Intl.DateTimeFormat('en-US').format(new Date(tix.dueDate || '')).toString();
+        } catch (err) {
+          ticketDueDate = 'No Date';
+        }
+        return [
+          tix.title ? `${tix.title?.substring(0, 40)}${tix.title.length > 40 ? '...' : ''}` : '',
+          tix.description ? `${tix.description?.substring(0, 80)}${tix.description.length > 80 ? '...' : ''}` : '',
+          tix.createdBy ? `${tix.createdBy?.firstName} ${tix.createdBy?.lastName}` : 'N/A',
+          tix.assignedTo ? `${tix.assignedTo?.firstName} ${tix.assignedTo?.lastName}`: 'Unassigned',
+          tix.status || 'N/A',
+          tix.priority?.toString() || 'N/A',
+          ticketDueDate,
+        ];
+      });
+      setTicketsData(formattedData);
     },
     onError: (error: ApolloError) => {
       if ((error.networkError as ServerParseError).statusCode === 401) {
@@ -41,7 +60,16 @@ export default function TicketView() {
     },
   });
 
+  const columns = ['Title', 'Description', 'Created By', 'Assigned To', 'Status', 'Priority', 'DueDate'];
+
   return (
-    <h1>Tickets</h1>
+    <div>
+      <MUIDataTable
+        title='All Tickets'
+        data={ticketsData}
+        columns={columns}
+        options={{ filterType: 'checkbox' }}
+      />
+    </div>
   );
 }
