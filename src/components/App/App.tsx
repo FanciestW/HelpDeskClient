8d340c12a0f7f885b5c4d82fe65fb2ee27028906
William Lin <wlin26@yahoo.com>
2020-04-24 12:24:40 -0400

    Updated App with CssBaseline and updated redirect paths

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { ApolloProvider, ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 import { ThemeProvider } from '@material-ui/styles';
-import { makeStyles, responsiveFontSizes, createMuiTheme } from '@material-ui/core';
+import { makeStyles, responsiveFontSizes, createMuiTheme, CssBaseline } from '@material-ui/core';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import { IRootReducer } from '../../redux/IRootReducer';
 import Dashboard from '../Dashboard/Dashboard';
 import TicketView from '../TicketView/TicketView';
+import NewTicketView from '../NewTicketView/NewTicketView';
 import Navbar from '../Navbar/Navbar';
 import NotFound from '../NotFound/NotFound';
 import SignUp from '../SignUp/SignUp';
@@ -40,25 +41,29 @@ const App: React.FC = () => {
 
   return (
     <ThemeProvider theme={theme}>
+      < CssBaseline />
       <div className={classes.root} style={{ backgroundColor: theme.palette.background.default }}>
         <Router>
           <ApolloProvider client={GraphQLClient}>
             {isAuthed ? <Navbar /> : null}
             <Switch>
               <Route path='/dashboard'>
-                {isAuthed ? <Dashboard /> : <Redirect to='login' />}
+                { isAuthed ? <Dashboard /> : <Redirect to='/login' /> }
               </Route>
-              <Route path='/tickets'>
-                {isAuthed ? <TicketView /> : <Redirect to='login' />}
+              <Route path='/tickets/test'>
+                { isAuthed ? <TicketView /> : <Redirect to='/login' /> }
+              </Route>
+              <Route path='/ticket/new'>
+                { isAuthed ? <NewTicketView /> : <Redirect to='/login' /> }
               </Route>
               <Route path='/login'>
-                {!isAuthed ? <Login /> : <Redirect to='dashboard' />}
+                { !isAuthed ? <Login /> : <Redirect to='/dashboard' /> }
               </Route>
               <Route path='/signup'>
-                {!isAuthed ? <SignUp /> : <Redirect to='dashboard' />}
+                { !isAuthed ? <SignUp /> : <Redirect to='/dashboard' /> }
               </Route>
               <Route exact path='/'>
-                <Redirect to='dashboard' />
+                <Redirect to='/dashboard' />
               </Route>
               <Route path='/*' component={NotFound} />
             </Switch>
